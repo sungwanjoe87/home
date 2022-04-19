2fe12e659e449905cc17c758ec3b9c40b10e60af
seungwanjoe87 <mayitshe@naver.com>
2022-04-19 21:22:12 +0900

    all commit

---
+++ 05.Web/w0420/w0420_01.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"}
+url = ""
+res = requests.get(url,headers=headers)
+soup = BeautifulSoup(res.text,"lxml")
