4c2fb849d885c69c31c31dfd0e266c2542e7689d
sungwanjoe87 <97008972+sungwanjoe87@users.noreply.github.com>
2022-05-16 18:17:27 +0900

    Create 01.test.py
    숙제 올릴것.

---
+++ 09.Django/01.test.py
@@ -0,0 +1,33 @@
+# boardpjt
+# board -> app카테고리 만들기.
+
+# boardList.html 페이지 띄우기.
+
+
+# tboard 테이블 설계 한것.
+# DB에 반영해 볼 것.
+
+#실습방에 올릴것.
+
+# * vs code 설정에서 setting 찾아서 추가 할것.
+
+
+# "auto-close-tag.activationOnLanguage": [
+#         "xml",
+#         "php",
+#         "blade",
+#         "ejs",
+#         "jinja",
+#         "javascript",
+#         "javascriptreact",
+#         "typescript",
+#         "typescriptreact",
+#         "plaintext",
+#         "markdown",
+#         "vue",
+#         "liquid",
+#         "erb",
+#         "lang-cfml",
+#         "cfml",
+#         "HTML (Eex)"
+#     ]
